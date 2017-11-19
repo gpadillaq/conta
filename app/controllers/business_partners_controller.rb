1ff0dd096f87d30f503b0f53353b5a523dd28890
Gerald Padilla <gpadillaq@gmail.com>
2017-11-18 21:13:38 -0600

    Agregado partial para manejo de acciones. Modificado el helper para que los inputs que no necesitan html_option, se agreguen en el campo de options. Agregado el show de business_partners. Modificado el manejador de errores para los formularios.

--- app/controllers/business_partners_controller.rb
+++ app/controllers/business_partners_controller.rb
@@ -8,6 +8,6 @@ class BusinessPartnersController < ApplicationController
   ##
   # Método de encapsulacion de los parametros permitodos en este controller.
     def business_partner_params
-      params.require(:business_partner).permit(:name, :business_partner_type_id, :coin_id, :telephone, :cellphone, :email, :active, :direction, :identificador)
+      params.require(:business_partner).permit(:name, :business_partner_type_id, :coin_id, :telephone, :cellphone, :email, :active, :direction, :id_number)
     end
 end
